e23cf0a55a2728f6e1f30b8338676f0f1a745e08
ilya.postnikov <ilya.postnikov@cybersteel.com>
2025-04-03 16:29:51 +0500

    master

---
+++ CssFilters.CommandManager/Helpers/CommandAttributesHelepr.cs
@@ -0,0 +1,21 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Commands;
+
+namespace CssFilters.CommandManager.Helpers
+{
+	/// <summary>
+	/// Помощник по атрибутам, которые предназначены командам.
+	/// </summary>
+	public static class CommandAttributesHelepr
+	{
+		/// <summary>
+		/// Проверяет команду на валидность <see cref="CommandHelperAttribute"/>.
+		/// </summary>
+		/// <param name="player">Игрок.</param>
+		/// <param name="info"><see cref="CommandInfo"/>.</param>
+		public static void CommandValidate(CCSPlayerController? player, CommandInfo info)
+		{
+
+		}
+	}
+}
